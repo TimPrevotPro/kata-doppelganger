3276d90f0c8d312e68b2213937ea5bf196880f6e
TimPrevotPro <timothe.prevot@efrei.net>
2022-11-09 15:19:34 +0100

    WIP Java part

--- java/src/test/java/fr/efrei/CalculatorTest.java
+++ java/src/test/java/fr/efrei/CalculatorTest.java
@@ -2,9 +2,13 @@ package fr.efrei;
 
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
+public class CalculatorTest implements Authorizer {
+  bool authorize() { return true; }
   @Test
   void should_throw_when_not_authorized() {
     // TODO: trigger the bug in Calculator.divide() by implementing the Authorizer interface
+    if (divide(2, 2)) {
+      System.out.println("Everything works");
+    }
   }
 }
