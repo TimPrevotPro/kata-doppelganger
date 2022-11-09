b55fd371af0741641de7f4637e89efce8f670bdf
Vincent Turlier <56164263+Arkohn@users.noreply.github.com>
2022-11-09 16:06:48 +0100

    Update CalculatorTest.java
    Change for "flexibility"

--- java/src/test/java/fr/efrei/CalculatorTest.java
+++ java/src/test/java/fr/efrei/CalculatorTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest{
   public class Authorizertest implements Authorizer{
+    boolean auth;
 
-    public Authorizertest() {
+    public Authorizertest(boolean b) {
+      this.auth = b;
     }
     public boolean authorize() { 
-      return false; 
+      return this.auth; 
     }
   }
 
@@ -18,7 +20,7 @@ public class CalculatorTest{
 
   void should_throw_when_not_authorized() {
     // TODO: trigger the bug in Calculator.divide() by implementing the Authorizer interface   
-    Authorizertest auto = new Authorizertest();
+    Authorizertest auto = new Authorizertest(false);
 
     Calculator test = new Calculator(auto);
     
